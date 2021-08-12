202bab4087ac70644771517691e4cc77856893e7
AdrianoGalan <dricogalan@gmail.com>
2021-08-12 02:40:23 -0300

    inicio rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'os', loadChildren: () => import('./os/os.module').then(m => m.OsModule) },
+  { path: 'funcionario', loadChildren: () => import('./funcionario/funcionario.module').then(m => m.FuncionarioModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
