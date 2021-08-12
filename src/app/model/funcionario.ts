ccc1425bae2b5799b9b873606c207e8ff2683559
AdrianoGalan <dricogalan@gmail.com>
2021-08-12 02:55:34 -0300

    inicio model

---
+++ src/app/model/funcionario.ts
@@ -0,0 +1,9 @@
+import { Pessoa } from './pessoa';
+
+export class Funcionario {
+
+  ra!: number;
+  pessoa!: Pessoa;
+  dataAdm!: Date;
+  funcao!: string;
+}
---
+++ src/app/model/usuario.ts
@@ -0,0 +1,8 @@
+import { Funcionario } from './funcionario';
+export class Usuario {
+
+  login!: string;
+  senha!: string;
+  permissao!: number;
+  funcionario!: Funcionario
+}
