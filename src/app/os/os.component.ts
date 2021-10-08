60529f95eebee2c6ec0c3217e221af8bf2544401
Adriano Galan <dricoo@hotmail.com>
2021-10-08 14:42:48 -0300

    listando os

--- src/app/os/os.component.ts
+++ src/app/os/os.component.ts
@@ -1,4 +1,10 @@
+import { catchError } from 'rxjs/operators';
+import { OsService } from './os.service';
+import { Os } from './../model/os';
+import { empty, Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { AlertModalComponent } from '../shared/alert-modal/alert-modal.component';
 
 @Component({
   selector: 'app-os',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OsComponent implements OnInit {
 
-  constructor() { }
+  os$!: Observable<Os[]>;
+
+  bsModalRef!: BsModalRef;
+
+  constructor(
+    private osService: OsService,
+    private modalService: BsModalService
+  ) { }
 
   ngOnInit(): void {
+    this.os$ = this.osService.list().pipe(
+      catchError(error => {
+        this.handleError();
+        return empty();
+      }
+
+      )
+    )
+  }
+
+  handleError(){
+    this.bsModalRef = this.modalService.show(AlertModalComponent);
+    this.bsModalRef.content.type = 'danger';
+    this.bsModalRef.content.message = 'Erro ao carregar';
   }
 
 }
